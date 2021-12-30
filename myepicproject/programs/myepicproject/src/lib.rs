c86e7a50428a3deda3c689574522adfe592cbf7b
Nissan Dookeran <nissan.dookeran@gmail.com>
2021-12-31 01:50:46 +1000

    fetches the account

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -6,9 +6,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialise total_gifs.
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
+// Attach certain variables to the Initialise content
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space=9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Tell Solana what we want to store on this account
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64
+}
--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,14 +1,35 @@
 const anchor = require('@project-serum/anchor');
 
+// Meed tje system program
+const { SystemProgram } = anchor.web3;
+
 describe('myepicproject', () => {
 
+  console.log("🚀 Starting test...");
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+
+  const baseAccount = anchor.web3.Keypair.generate();
 
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.Myepicproject;
-    const tx = await program.rpc.initialize();
+    const tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
     console.log("Your transaction signature", tx);
+
+    // Fetch data from the account
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 GIF Count', account.totalGifs.toString());
   });
 });
